8903a673cacca00ed1c2339e7de856b2880c67ab
Carl Wheezer <slushern42@gmail.com>
2022-04-09 14:20:01 -0700

    updated notesRouter.js

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,39 +1,39 @@
 const fs = require('fs');
 const path = require('path')
-const notes = require('../../db/db.json')
+const { notes } = require('../../db/db.json')
 const router = require('express').Router();
 const uniqid = require('uniqid')
+const { createNewNote, findById, editNote, removeNote } = require('../../lib/notes')
 
 // get all the notes from db/db.json
 router.get('/notes', (req, res) => {
  let results = notes
- res.send(results)
+ res.json(results)
 })
 
-// create notes
+// create new notes or edit an existing note if id already exists
 router.post('/notes', (req, res) => {
- let id = uniqid()
-
-notes.push({
- id: id,
- title: req.body.title,
- text: req.body.text, 
-})
-// write new note to db/db.json
-fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(notes), err => {
- if (err) {
-  console.log(err)
+ // npm uniqid unique id generator
+ if (!req.body.id) {
+  req.body.id = uniqid();
+  createNewNote(req.body, notes)
  } else {
-  res.json({
-   message: "added successfully",
-   note: {
-    id: id,
-    title: req.body.title,
-    text: req.body.text
-   }
- })
+  editNote(req.body, notes)
  }
-})
+ //send as json
+ res.json(req.body)
 }) 
+
+
+// Delete note
+router.delete('/notes/:id', (req, res) => {
+ const note = findById(req.params.id, notes)
+
+  removeNote(note, notes)
+ 
+})
+
+//DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
 
 module.exports = router;
